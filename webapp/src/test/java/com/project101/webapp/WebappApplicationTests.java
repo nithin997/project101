238ac65d82cb8d306e9690936f17eafd770c3064
student <nithin.nagendra@simplilearn.net>
2021-03-21 12:41:03 +0000

    Project Skeleton

---
+++ webapp/src/test/java/com/project101/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project101.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
